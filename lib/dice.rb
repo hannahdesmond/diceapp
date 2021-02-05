b767c068bbf3f98d5af7f5812f93271319a0453d
Max Clarke <maxclarke@MA368.local>
2021-02-05 10:54:15 +0000

    random roll implemeneted

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,20 +1,5 @@
 class Dice
-  def start
-    dice = Dice.new
-    puts "Welcome to Dice Game, how many dice would you like to roll?"
-    dice_amount = gets.chomp
-  end
-
-  def roll
-    dice_amount.times {current_roll = [1,2,3,4,5,6].sample}
-    puts current_roll
-  end
-
-  def history
-
-  end
-
-  def total
-
+  def roll(amount)
+    dice_roll = [1,2,3,4,5,6].sample
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,17 +1,11 @@
-require 'dice'
+require 'dice.rb'
 
 describe Dice do
-    it 'allows the user to roll the dice' do
-        expect(subject).to respond_to(:roll)
-    end
-    it 'returns a number from the range of the dice' do
-        expect(1..6).to cover(subject.roll)
-    end
-    it 'allows the user to see roll history' do
-        expect(subject).to respond_to(:history)
-    end
-    it 'allows the user to see roll total' do
-        expect(subject).to respond_to(:total)
-    end
+  it 'dice responds to roll' do
+    expect(subject).to respond_to(:roll)
+  end
+  it 'returns a number between 1-6' do
+    expect(1..6).to cover(subject.roll)
+  end
 
 end
